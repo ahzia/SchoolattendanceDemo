c372d6d30f3064e8f427a7e6e0fbc1084a5e4814
Ahmad Zia <ah.ziayosfi@gmail.com>
2019-04-28 12:40:42 +0430

    Initial commit

---
+++ app/src/main/java/com/example/ahmad/schoolschedualdemo/viewDataBase.java
@@ -0,0 +1,13 @@
+package com.example.ahmad.schoolschedualdemo;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+public class viewDataBase extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.view_database_layout);
+    }
+}
